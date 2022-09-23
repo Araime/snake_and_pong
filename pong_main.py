bf9dd9d6edd7845eee5f8ecace293972e6aec641
Nick Timofeev <alvonsa09121985@gmail.com>
2022-09-23 11:11:46 +0400

    Fix typos

--- pong_main.py
+++ pong_main.py
@@ -30,37 +30,37 @@ def start_pong_game():
         time.sleep(ball.move_speed)
         ball.move()
 
-        '''Detect collision with wall'''
+        # Detect collision with wall
         if ball.ycor() > 280 or ball.ycor() < -280:
             ball.bounce_y()
 
-        '''Detect collision with paddles'''
+        # Detect collision with paddles
         if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
             ball.bounce_x()
 
-        '''Move right paddle'''
+        # Move right paddle
         if watched_keys[0].down and not watched_keys[1].down and not r_paddle.ycor() > 240:
             r_paddle.go_up()
         elif not watched_keys[0].down and watched_keys[1].down and not r_paddle.ycor() < -220:
             r_paddle.go_down()
 
-        '''Move left paddle'''
+        # Move left paddle
         if watched_keys[2].down and not watched_keys[3].down and not l_paddle.ycor() > 240:
             l_paddle.go_up()
         elif not watched_keys[2].down and watched_keys[3].down and not l_paddle.ycor() < -220:
             l_paddle.go_down()
 
-        '''Detect r_paddle misses'''
+        # Detect r_paddle misses
         if ball.xcor() > 380:
             ball.reset_position()
             score.l_point()
 
-        '''Detect l_paddle misses'''
+        # Detect l_paddle misses
         if ball.xcor() < -380:
             ball.reset_position()
             score.r_point()
 
-        '''Stop the game when one player's score is 10'''
+        # Stop the game when one player's score is 10
         if score.l_score == 10 or score.r_score == 10:
             game_is_on = False
             score.game_over()
